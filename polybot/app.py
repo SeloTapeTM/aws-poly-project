1a4a8c81d76e59c5f69cf5a846ff39c3cd3d2229
BuildTools <unconfigured@null.spigotmc.org>
2024-02-03 19:52:36 +0200

    init polyapp.py added secret handling

---
+++ polybot/app.py
@@ -0,0 +1,77 @@
+import flask
+from flask import request
+import os
+from bot import ObjectDetectionBot
+import json
+
+app = flask.Flask(__name__)
+
+
+# Static Helper Methods
+def get_secret():
+    secret_name = "omerd-secret-tg"
+    region_name = "eu-central-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    return json.loads(secret)
+
+
+# TODO load TELEGRAM_TOKEN value from Secret Manager
+secrets = get_secret()
+TELEGRAM_TOKEN = secrets["TELEGRAM_TOKEN"]  # os.environ['TELEGRAM_TOKEN']
+
+TELEGRAM_APP_URL = secrets["TELEGRAM_APP_URL"]  # os.environ['TELEGRAM_APP_URL']
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'Ok'
+
+
+@app.route(f'/{TELEGRAM_TOKEN}/', methods=['POST'])
+def webhook():
+    req = request.get_json()
+    bot.handle_message(req['message'])
+    return 'Ok'
+
+
+@app.route(f'/results/', methods=['GET'])
+def results():
+    prediction_id = request.args.get('predictionId')
+
+    # TODO use the prediction_id to retrieve results from DynamoDB and send to the end-user
+
+    chat_id = ...
+    text_results = ...
+
+    bot.send_text(chat_id, text_results)
+    return 'Ok'
+
+
+@app.route(f'/loadTest/', methods=['POST'])
+def load_test():
+    req = request.get_json()
+    bot.handle_message(req['message'])
+    return 'Ok'
+
+
+if __name__ == "__main__":
+    bot = ObjectDetectionBot(TELEGRAM_TOKEN, TELEGRAM_APP_URL)
+
+    app.run(host='0.0.0.0', port=8443)
