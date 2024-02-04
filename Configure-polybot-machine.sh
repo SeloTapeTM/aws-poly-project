3a8089741e4dc788bbd52a404822d572b7b10776
BuildTools <unconfigured@null.spigotmc.org>
2024-02-04 18:55:38 +0200

    fixed polymachine conf script

--- Configure-polybot-machine.sh
+++ Configure-polybot-machine.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# ARGS
+ARG=$1
+VER="latest"
+
 # Update repositories
 sudo apt update
 
@@ -11,4 +15,4 @@ sudo sh get-docker.sh
 rm -f get-docker.sh
 
 # Run docker container for Polybot
-sudo docker run --restart=always <"INSERT IMAGE NAME AND VERSION HERE">
+sudo docker run -p 8443:8443 --restart=always selotapetm/polybotaws:${ARG:-$VER}
